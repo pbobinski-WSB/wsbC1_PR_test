04f9a996eae7b9d27b5c0155e6ad322eb5486e29
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2022-05-12 13:56:01 +0200

    1szy commit pbobinsk

---
+++ src/wsb/cw/prtest/Klasa3pbobinsk.java
@@ -0,0 +1,25 @@
+package wsb.cw.prtest;
+
+public class Klasa3pbobinsk {
+
+    private String pole = "Pole";
+
+    public String getPole() {
+        return pole;
+    }
+
+    public Klasa3pbobinsk(String pole) {
+        this.pole = pole;
+    }
+
+    public void setPole(String pole) {
+        this.pole = pole;
+    }
+
+    @Override
+    public String toString() {
+        return "Klasa3pbobinsk{" +
+                "pole='" + pole + '\'' +
+                '}';
+    }
+}
